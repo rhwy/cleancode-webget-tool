fe3ab2c62c6fa8033c1510a65c903c9c105e7cb0
lienafalionel <lienafa.lionel@gmail.com>
2015-04-07 14:30:02 +0200

    Gestion commandes sans gestion exceptions

--- Students/lienafa-lionel/nget-v1/nget-v1/Program.cs
+++ Students/lienafa-lionel/nget-v1/nget-v1/Program.cs
@@ -7,6 +7,7 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Net;
 
 namespace nget_v1
 {
@@ -14,13 +15,43 @@ namespace nget_v1
 	{
 		public static void Main(string[] args)
 		{
+			WebClient client = new WebClient();
+			string url = args[2];
+			
 			switch(args[0]) {
 				case "get":
 					if(args[1] == "-url") {
-						Console.WriteLine("Adresse: " + args[2]);
+						
+						if(args[3] == "-save") {
+							client.DownloadFile(url, args[4]);
+						} else {
+							Console.WriteLine(client.DownloadString(url));
+						}
 					}
 					break;
 				case "test":
+					int nbTimes = Convert.ToInt16(args[4]);
+					double[] timesArray = new double[nbTimes];
+					double cumul = 0;
+					for(int i = 0; i < nbTimes; i++)
+					{
+						DateTime TimeStart = DateTime.Now;
+						client.DownloadString(url);
+						TimeSpan TimeDif = DateTime.Now.Subtract(TimeStart);
+						timesArray[i] = TimeDif.TotalSeconds;
+						cumul += TimeDif.TotalSeconds;
+						if(args.Length < 5) {
+							if(args[5] != "-avg") {
+								Console.WriteLine(TimeDif.TotalSeconds);
+							}
+						}
+					}
+					
+					if(args.Length < 5) {
+						if(args[5] == "-avg") {
+							Console.WriteLine(cumul / nbTimes);
+						}
+					}
 					break;
 				default:
 					break;
