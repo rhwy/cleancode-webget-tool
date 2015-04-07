ee4b88fa505a9e9bcd97e7dc974d9a317f32bfa8
Valentin <valmartins@giti.com>
2015-04-07 12:28:40 +0200

    Ajout du projet

---
+++ Students/martins-valentin/nget-v1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Valentin
+ * Date: 07/04/2015
+ * Time: 12:14
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace nget_v1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			if(string.IsNullOrEmpty(args))
+				Console.WriteLine("Arguments error");
+			
+			
+			
+			
+			Console.ReadKey(true);
+		}
+	}
+}
