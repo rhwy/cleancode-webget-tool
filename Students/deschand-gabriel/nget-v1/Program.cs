41d1a35eca385f6fa805cb748a7a39fd61726260
gabistoufly <deschand.gabriel@gmail.com>
2015-04-07 12:06:16 +0200

    Create project

---
+++ Students/deschand-gabriel/nget-v1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Gabi
+ * Date: 07/04/2015
+ * Time: 12:02
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace nget_v1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.ReadKey(true);
+		}
+	}
+}
