4dc44da8a5854258cd17a4d4d8246f79f4cbfcf5
denam60 <denam60@hotmail.fr>
2015-04-07 12:30:55 +0200

    First commit

---
+++ Students/DIRAME-Damien/nget-v1/NGET/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Damien
+ * Date: 07/04/2015
+ * Time: 12:03
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace NGET
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			if (args[0]=="get"){
+				Console.WriteLine("oui");
+			}
+		}
+	}
+}
