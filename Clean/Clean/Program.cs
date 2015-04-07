a4202b342c20dd5b82d3f86bb26aa12df31b46da
Ludo <bissoly.ludovik@gmail.com>
2015-04-07 12:23:15 +0200

    Start project

---
+++ Clean/Clean/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: ludovik
+ * Date: 07/04/2015
+ * Time: 12:21
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Clean
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
