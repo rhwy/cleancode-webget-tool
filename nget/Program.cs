e998110489fc5bd20a7b18a164f35439cd659aa6
ChoucheneH <hdili.chouchene@gmail.com>
2015-04-07 12:21:32 +0200

    Mon premier Commit

---
+++ nget/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Crée par SharpDevelop.
+ * Utilisateur: hedhili
+ * Date: 07/04/2015
+ * Heure: 12:20
+ * 
+ * Pour changer ce modèle utiliser Outils | Options | Codage | Editer les en-têtes standards.
+ */
+using System;
+
+namespace nget
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("mon premier commit.. ");
+			Console.ReadKey(true);
+		}
+	}
+}
