74ac3eac4d5ae753e9307a004f395e859a4bf491
Pere-Dudo <drobynhotmail.fr>
2015-04-07 14:57:28 +0200

    Fini 50%

--- Students/plancon-robin/nget-v1/nget-v1/Program.cs
+++ Students/plancon-robin/nget-v1/nget-v1/Program.cs
@@ -12,7 +12,7 @@ namespace ngetv1
 				throw new Exception ();
 			}
 
-			if (args.Length != 3 && args.Length != 5) {
+			if (args.Length != 3 && args.Length != 5 && args.Length != 6) {
 				throw new Exception ();
 			}
 
@@ -21,28 +21,55 @@ namespace ngetv1
 			String url = args [2];
 			String ngetOption = null;
 			String other = null;
+			String moy = null;
 
 			if (args.Length == 5) {
 				ngetOption = args [3];
 				other = args [4];
 			}
-			if (method == "get" && comp == "-url" && ngetOption != null) {
-				
+			if(args.Length == 6) {
+				ngetOption = args [3];
+				other = args [4];
+				moy = args [5];
 			}
-			else if (method == "get" && comp == "-url") {
-				
-				HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create (url);
-				myRequest.Method = "GET";
-				WebResponse myResponse = myRequest.GetResponse ();
-				if (myResponse == null) {
-					throw new Exception ();
-				}
-				StreamReader sr = new StreamReader (myResponse.GetResponseStream (), System.Text.Encoding.UTF8);
-				string result = sr.ReadToEnd ();
-				sr.Close ();
-				myResponse.Close ();
 
-				Console.WriteLine (result);
+			if (method == "get" && comp == "-url") {
+				if (ngetOption != null) {
+					HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create (url);
+					myRequest.Method = "GET";
+					WebResponse myResponse = myRequest.GetResponse ();
+					if (myResponse == null) {
+						throw new Exception ();
+					}
+					StreamReader sr = new StreamReader (myResponse.GetResponseStream (), System.Text.Encoding.UTF8);
+					string result = sr.ReadToEnd ();
+					sr.Close ();
+					myResponse.Close ();
+
+
+
+					StreamWriter monStreamWriter = new StreamWriter("./"+other,true); 
+					monStreamWriter.WriteLine (result);
+					monStreamWriter.Close();
+					
+				} else {
+				
+					HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create (url);
+					myRequest.Method = "GET";
+					WebResponse myResponse = myRequest.GetResponse ();
+					if (myResponse == null) {
+						throw new Exception ();
+					}
+					StreamReader sr = new StreamReader (myResponse.GetResponseStream (), System.Text.Encoding.UTF8);
+					string result = sr.ReadToEnd ();
+					sr.Close ();
+					myResponse.Close ();
+
+					Console.WriteLine (result);
+				}
+			}
+			else if (method == "test" && comp == "-url"){
+
 			}
 
 			Console.ReadKey (true);
