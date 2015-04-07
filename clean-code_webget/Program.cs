65601cb06554c779da0c41430814c574edcae413
neimad971 <damientalbot26@gmail.com>
2015-04-07 12:02:11 +0200

    initial commit

---
+++ clean-code_webget/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace cleancode_webget
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("Hello World!1");
+		}
+	}
+}
