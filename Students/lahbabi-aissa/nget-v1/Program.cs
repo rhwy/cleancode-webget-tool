487dcf5cba1ca9ac6bac42f8d83983c67f2cce1c
Aissa <aissa_git@installtech.fr>
2015-04-07 12:26:30 +0200

    Initial commit

---
+++ Students/lahbabi-aissa/nget-v1/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Crée par SharpDevelop.
+ * Utilisateur: Aissa
+ * Date: 07/04/2015
+ * Heure: 11:42
+ * 
+ * Pour changer ce modèle utiliser Outils | Options | Codage | Editer les en-têtes standards.
+ */
+using System;
+using System.Net;
+
+namespace nget_v1
+{
+	class Program
+	{
+		public static void printHelp() {
+			Console.Write("Utilisataion : ...");
+			Console.ReadKey(true);
+		}
+		
+		public static void Main(string[] args)
+		{
+			if (args.Length < 2 || (args[0] != "get" && args[0] == "test")) {
+				printHelp();
+				return;
+			}
+			
+			if (args[0] == "get") {
+				
+			} else if (args[0] == "test") {
+			
+			} else {
+				printHelp();
+			}
+			
+			var webRequest = WebRequest.Create(@"http://api.openweathermap.org/data/2.5/weather?q=paris&units=metric");
+			
+
+			
+			Console.ReadKey(true);
+		}
+		
+		
+	}
+}
---
+++ Students/lahbabi-aissa/nget-v1/WebFile.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Crée par SharpDevelop.
+ * Utilisateur: Aissa
+ * Date: 07/04/2015
+ * Heure: 12:15
+ * 
+ * Pour changer ce modèle utiliser Outils | Options | Codage | Editer les en-têtes standards.
+ */
+using System;
+
+namespace nget_v1
+{
+	/// <summary>
+	/// Description of WebFile.
+	/// </summary>
+	public class WebFile
+	{
+		string _url;
+		
+		public WebFile(string url)
+		{
+			_url = url;
+		}
+		
+		
+		public void print(string url) {
+			
+		}
+		
+		public Boolean download(string url, string filename) {
+			
+			return false;
+		}
+		
+		public Boolean times(string url, int nb_loads, Boolean print_avg) {
+			
+			return false;
+		}
+		
+		
+		
+		
+	}
+}
