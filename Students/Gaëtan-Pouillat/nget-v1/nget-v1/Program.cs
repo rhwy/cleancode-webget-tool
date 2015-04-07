44181409fc2ddef15c8d4140b202c831a98b3017
gaetandugheto <pouillat.gaetan@gmail.com>
2015-04-07 12:44:37 +0200

    commit 1

--- Students/Gaëtan-Pouillat/nget-v1/nget-v1/Program.cs
+++ Students/Gaëtan-Pouillat/nget-v1/nget-v1/Program.cs
@@ -2,7 +2,7 @@
  * Created by SharpDevelop.
  * User: Griev_000
  * Date: 07/04/2015
- * Time: 12:12
+ * Time: 12:41
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
@@ -15,17 +15,12 @@ namespace nget_v1
 		public static void Main(string[] args)
 		{
 			if(args.Length == 0){
-				Console.WriteLine("Veuillez saisir des parametres");
-			}else{
-				if(args[0] == "get"){
-					if(args[1] == "-url"){
-						Console.WriteLine("test");
-					}
-				}else if(args[0] == "test"){
-				
+				Console.WriteLine("saisir parametre");
+			}else if(args[0] == "get"){
+				if(args[0] == "-get"){
+					
 				}
 			}
-
 		}
 	}
 }
