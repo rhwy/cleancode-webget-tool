e21034af13b5b32e263942160000793813bd5894
gaetandugheto <pouillat.gaetan@gmail.com>
2015-04-07 14:52:53 +0200

    commit : programme fonctionnel

--- Students/Gaëtan-Pouillat/nget-v1/nget-v1/Program.cs
+++ Students/Gaëtan-Pouillat/nget-v1/nget-v1/Program.cs
@@ -7,18 +7,90 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.IO;
+using System.Net;
 
 namespace nget_v1
 {
 	class Program
 	{
+		public static String getContentByUrl(string url){
+			WebClient wc = new WebClient();
+			return wc.DownloadString(url);
+		}
+		
 		public static void Main(string[] args)
 		{
 			if(args.Length == 0){
+				
 				Console.WriteLine("saisir parametre");
+				
 			}else if(args[0] == "get"){
-				if(args[0] == "-get"){
+				
+				if(args.Length >= 2 && args[1] == "-url"){
 					
+					if(args.Length >= 5 && args[3] == "-save"){
+						
+						StreamWriter sw = new StreamWriter(args[4]);
+ 
+						sw.Write(getContentByUrl(args[2]));
+						sw.Close();
+						
+					}else{
+						
+						Console.WriteLine(getContentByUrl(args[2]));
+						
+					}
+				}
+				else{
+					Console.WriteLine("parametre de get : -url <url>");
+				}
+			}else if(args[0] == "test"){
+				
+				if(args.Length >=4 &&( args[1] == "-url" && args[3] == "-times")){
+					
+					int fois;
+					bool result = Int32.TryParse(args[4], out fois);
+					
+					if(args.Length == 6 && args[5] == "-avg"){
+						
+							if(result){
+								
+								long resultat = 0;
+								
+								for(int i=0;i<fois;i++){
+									
+									long milliseconds1 = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+									getContentByUrl(args[2]);
+									long milliseconds2 = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+									resultat += milliseconds2 - milliseconds1;
+									
+								}
+								
+								Console.WriteLine("moyenne temps chargement: " + resultat/fois);
+								
+							}else{
+								Console.WriteLine("parametre -time : entier attendu");
+							}
+					}
+					else{
+	      				
+						if(result){
+							
+							for(int i=0;i<fois;i++){
+								
+								long milliseconds1 = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+								getContentByUrl(args[2]);
+								long milliseconds2 = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+								long resultat = milliseconds2 - milliseconds1;
+								
+								Console.WriteLine("temps test numero" + i + " : " + resultat);
+							}
+							
+						}else{
+							Console.WriteLine("parametre -time : entier attendu");
+						}
+					}	
 				}
 			}
 		}
