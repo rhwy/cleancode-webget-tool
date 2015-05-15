daadc30f0a4b28bb0005394819b3c2ec88383e55
Lorpoce <lorpoce@gmail.com>
2015-05-15 22:46:53 +0200

    Corrections

--- Etape 2/nget-v1/GetTask.cs
+++ Etape 2/nget-v1/GetTask.cs
@@ -7,77 +7,74 @@ namespace ngetv1
 {
 	public class GetTask
 	{
-		private string sourceUrl { get; set;}
-		private string destUrl { get; set;}
+		private string sourceUrl { get; set; }
+		private string destUrl { get; set; }
 
-		public GetTask (string[] args)
+		public GetTask(string[] args)
 		{
-			DoGet (args);
+			InitializeAttributes(args);
+			DoGet();
 		}
-
-		private void DoGet (string[] args)
+		private void InitializeAttributes(string[] args)
 		{
+			for (int i = 0; i <= args.Length - 1; i++)
+			{
 
-			StringBuilder sb = new StringBuilder ();
-
-			// Gestion des paramètres
-			int length = args.Length;
-
-			InitializeAttributes (args, length);
-
-			if (sourceUrl == null || String.IsNullOrEmpty (sourceUrl)) {
-				throw new Exception (UsageUtils.GetStringUnknownParameter (sourceUrl));
-			}
-
-			var page = (new WebClient ()).DownloadString (sourceUrl);
-
-			PrintLogs (sb, sourceUrl, destUrl, page);
-		}
-
-		private void InitializeAttributes(string[] args, int length)
-		{
-			for (int i = 0; i <= length - 1; i++) {
-
-				if (args [i] == null || String.IsNullOrEmpty (args [i])) {
-					throw new Exception (UsageUtils.GetUsage ());
+				if (args[i] == null || String.IsNullOrEmpty(args[i]))
+				{
+					throw new Exception(UsageUtils.GetUsage());
 				}
 
-				if (args [i] == "-url") {
-					ArrayUtils.checkArrayLengthCorrect (i, length);
-					sourceUrl = args [i + 1];
-				} else if (args [i] == "-save") {
-					ArrayUtils.checkArrayLengthCorrect (i, length);
-					destUrl = args [i + 1];
+				if (args[i] == "-url")
+				{
+					ArrayUtils.checkArrayLengthCorrect(i, args.Length);
+					sourceUrl = args[i + 1];
+				}
+				else if (args[i] == "-save")
+				{
+					ArrayUtils.checkArrayLengthCorrect(i, args.Length);
+					destUrl = args[i + 1];
 				}
 			}
 		}
+
+		private void DoGet()    
+		{
+			if (sourceUrl == null || String.IsNullOrEmpty(sourceUrl))
+			{
+				throw new Exception(UsageUtils.GetStringUnknownParameter(sourceUrl));
+			}
+
+			StringBuilder sb = new StringBuilder();
+
+			var page = (new WebClient()).DownloadString(sourceUrl);
+
+			PrintLogs(sb, sourceUrl, destUrl, page);
+		}
+
 
 		private void PrintLogs(StringBuilder sb, string sourceUrl, string destUrl, string page)
 		{
-			sb.Append ("Printing content of " + sourceUrl);
-			sb.AppendLine ();
-			sb.Append (page);
-			sb.AppendLine ();
+			sb.Append("Printing content of " + sourceUrl);
+			sb.AppendLine();
+			sb.Append(page);
+			sb.AppendLine();
 
 			// Si l'URL de destination est renseignée
-			if (destUrl != null && !String.IsNullOrEmpty (destUrl)) {
-				sb.Append ("Saving to " + sourceUrl);
-				WriteToFile (destUrl, page);
+			if (destUrl != null && !String.IsNullOrEmpty(destUrl))
+			{
+				sb.Append("Saving to " + sourceUrl);
+				WriteToFile(destUrl, page);
 			}
 
-			WriteToConsole (sb);
-		}
-
-		private void WriteToConsole(System.Text.StringBuilder sb )
-		{
-			Console.WriteLine (sb);
+			Console.WriteLine(sb);
 		}
 
 		private void WriteToFile(string destUrl, string content)
 		{
-			TextWriter tw = new StreamWriter (destUrl, true);
-			tw.WriteLine (content);
-			tw.Close ();
+			TextWriter tw = new StreamWriter(destUrl, true);
+			tw.WriteLine(content);
+			tw.Close();
 		}
 	}
 }
--- Etape 2/nget-v1/TestTask.cs
+++ Etape 2/nget-v1/TestTask.cs
@@ -7,60 +7,73 @@ namespace ngetv1
 	public class TestTask
 	{
 
-		private string sourceUrl { get; set;}
-		private int time { get; set;}
-		private bool isAvg { get; set;}
+		private string sourceUrl { get; set; }
+		private int time { get; set; }
+		private bool isAvg { get; set; }
 
-		public TestTask (string[] args)
+		public TestTask(string[] args)
 		{
-			DoTest (args);
+			InitializeAttributes(args);
+			Console.WriteLine(DoTest(args));
 		}
 
-		private void DoTest (string[] args)
-		{
-			int length = args.Length;
-
-			InitializeAttributes (args, length);
-
-			TimeSpan totalDuration = new TimeSpan ();
-			for (int i = 0; i <= time; i++) {
-
-				DateTime before = DateTime.Now;
-				(new WebClient ()).DownloadString (sourceUrl);
-				DateTime after = DateTime.Now;
-
-				if (!isAvg) {
-					Console.WriteLine ("Duration (ms) : " + after.Subtract (before).TotalMilliseconds);
-				} else {
-					totalDuration += (after - before);
-				}
-			}
-
-			if (isAvg) {
-				Console.WriteLine ("Total duration average (ms) : " + totalDuration.TotalMilliseconds / time);
-			}
-		}
-
-		private void InitializeAttributes(string[] args, int length)
+		private void InitializeAttributes(string[] args)
 		{
 			// Récupèration des arguments
-			for (int i = 0; i <= length - 1; i++) {
-
-				if (args [i] == null || String.IsNullOrEmpty (args [i])) {
-					throw new Exception (UsageUtils.GetUsage ());
+			for (int i = 0; i <= args.Length - 1; i++)
+			{
+				if (args[i] == null || String.IsNullOrEmpty(args[i]))
+				{
+					throw new Exception(UsageUtils.GetUsage());
 				}
 
-				if (args [i] == "-url") {
-					ArrayUtils.checkArrayLengthCorrect (i, length);
-					sourceUrl = args [i + 1];
-				} else if (args [i] == "-times") {
-					ArrayUtils.checkArrayLengthCorrect (i, length);
-					time = Int32.Parse (args [i + 1]);
-				} else if (args [i] == "-avg") {
+				if (args[i] == "-url")
+				{
+					ArrayUtils.checkArrayLengthCorrect(i, args.Length);
+					sourceUrl = args[i + 1];
+				}
+				else if (args[i] == "-times")
+				{
+					ArrayUtils.checkArrayLengthCorrect(i, args.Length);
+					time = Int32.Parse(args[i + 1]);
+				}
+				else if (args[i] == "-avg")
+				{
 					isAvg = true;
 				}
 			}
 		}
+
+		private string DoTest(string[] args)
+		{
+			string chaine = "";
+
+			TimeSpan totalDuration = new TimeSpan();
+
+			for (int i = 0; i <= time; i++)
+			{
+				DateTime before = DateTime.Now;
+				(new WebClient()).DownloadString(sourceUrl);
+				DateTime after = DateTime.Now;
+
+				if (!isAvg)
+				{
+					chaine += "Duration (ms) : " + after.Subtract(before).TotalMilliseconds + Environment.NewLine;
+				}
+				else
+				{
+					totalDuration += (after - before);
+				}
+			}
+
+			if (isAvg)
+			{
+				chaine += "Total duration average (ms) : " + totalDuration.TotalMilliseconds / time;
+			}
+
+			return chaine;
+		}
+
 	}
 }
 
