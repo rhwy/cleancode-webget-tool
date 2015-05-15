feba61d5aad5d98fc426d8eed5d4a9dcdd0dc43c
hchaudhry <chaudhry.hussam@gmail.com>
2015-05-15 21:38:35 +0200

    Added test action

--- etape-2/Students/chaudhry-hussam/nget-v1/nget-v1/Program.cs
+++ etape-2/Students/chaudhry-hussam/nget-v1/nget-v1/Program.cs
@@ -12,54 +12,6 @@ namespace ngetv1
 			result = webGet.checkForArguments(args);
 			Console.WriteLine (result);
 			Console.ReadLine ();
-
-			/*
-			System.Net.WebClient client = new System.Net.WebClient();
-
-			if (args [0] == "get") {
-				if (args [1] == "-url") {
-
-					String webpage = client.DownloadString (args [2]);
-
-					if (args.Length >= 4 && args [3] == "-save") {
-						System.IO.File.WriteAllText (args [4], webpage);
-						return;
-					}
-
-					Console.WriteLine (webpage);
-				}
-			}
-
-			if (args [0] == "test") {
-				System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch ();
-
-				if (args [1] == "-url") {
-					if (args [3] == "-times") {
-				
-						int nb = 0;
-						TimeSpan compteur = new TimeSpan();
-						while (nb < Int32.Parse(args[4])) {
-							timer.Start ();
-							client.DownloadString (args [2]);
-							timer.Stop ();
-
-							TimeSpan ts = timer.Elapsed;
-
-							Console.WriteLine (ts);
-							compteur += ts;
-							timer.Reset ();
-							nb++;
-						}
-
-						if (args.Length >= 6 && args [5] == "-avg") {
-							//Console.WriteLine(compteur.TotalMilliseconds / Int32.Parse(args[4]));
-						}
-					}
-
-
-				}
-
-			}*/
 		}
 	}
 
@@ -111,7 +63,15 @@ namespace ngetv1
 
 		public string actionTest(string[] args)
 		{
-			return "ss";
+			string result = string.Empty;
+
+			if (!checkURLArgument (args [1]) && args [2] != null) {
+
+				if (args [3] == "-times" && args[4] != null) {
+					result = getURLMultipleTime (args [2], args [4]);
+				}
+			}
+			return result;
 		}
 
 		public bool checkURLArgument(string urlAgument)
@@ -126,6 +86,29 @@ namespace ngetv1
 		{	
 			/// à isoler
 			System.IO.File.WriteAllText (path, toWrite);
+		}
+
+		public string getURLMultipleTime(string url, string multiple)
+		{
+			System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch ();
+			System.Net.WebClient client = new System.Net.WebClient ();
+			string result = string.Empty;
+			int nb = 0;
+			TimeSpan compteur = new TimeSpan();
+			while (nb < Int32.Parse(multiple)) {
+				timer.Start ();
+				client.DownloadString (url);
+				timer.Stop ();
+
+				TimeSpan ts = timer.Elapsed;
+
+				result += ts + Environment.NewLine;
+				compteur += ts;
+				timer.Reset ();
+				nb++;
+			}
+
+			return result;
 		}
 	}
 }
