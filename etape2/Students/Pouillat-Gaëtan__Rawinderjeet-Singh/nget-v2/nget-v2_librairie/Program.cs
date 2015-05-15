111795cd4e1a80105134e33f83c85cbdad643ab2
Gaga94 <pouillat.gaetan@gmail.com>
2015-05-15 18:41:43 +0200

    Refactoring projet + debut test

---
+++ etape2/Students/Pouillat-Gaëtan__Rawinderjeet-Singh/nget-v2/nget-v2_librairie/Program.cs
@@ -0,0 +1,135 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Griev_000
+ * Date: 07/04/2015
+ * Time: 12:41
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Net;
+
+namespace nget_v2
+{
+	class Program
+	{
+		public Program()
+		{
+			
+		}	
+		
+		public String getContentByUrl(string url){
+			WebClient wc = new WebClient();
+			string content = "";
+			try {
+				content = wc.DownloadString(url);
+			}catch (Exception) {
+				
+				content = "<h1>hello</h1>";
+			}
+			return content;
+		}
+		
+		public void writeUrlContentInFile(string contentUrl, string path){
+			try{		
+				StreamWriter sw = new StreamWriter(path);	
+				sw.Write(contentUrl);
+				sw.Close();
+							
+			}catch(Exception ex){
+				Console.WriteLine(ex);
+			}
+		}
+		
+		public static void Main(string[] args)
+		{
+			Program prog = new Program();
+			if(args.Length == 0){
+				
+				Console.WriteLine("saisir parametre");
+				
+			}else if(args[0] == "get"){
+				
+				if(args.Length >= 2 && args[1] == "-url"){
+					
+					if(args.Length >= 5 && args[3] == "-save"){
+												
+						prog.writeUrlContentInFile(prog.getContentByUrl(args[2]),args[4]);
+					}else{
+						
+						try{
+							Console.WriteLine(prog.getContentByUrl(args[2]));
+						}catch(Exception ex){
+							Console.WriteLine(ex);
+						}
+					}
+				}
+				else{
+					Console.WriteLine("parametre de get : -url <url>");
+				}
+			}else if(args[0] == "test"){
+				
+				if(args.Length >=4 &&( args[1] == "-url" && args[3] == "-times")){
+					
+					int fois;
+					bool result = Int32.TryParse(args[4], out fois);
+					
+					if(args.Length == 6 && args[5] == "-avg"){
+						
+							if(result){
+								
+								long resultat = 0;
+								
+								for(int i=0;i<fois;i++){
+									
+									long milliseconds1 = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+									
+									try{
+										prog.getContentByUrl(args[2]);
+									}
+									catch(Exception ex){
+										Console.WriteLine(ex);
+									}
+									
+									long milliseconds2 = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+									resultat += milliseconds2 - milliseconds1;
+									
+								}
+								
+								Console.WriteLine("moyenne temps chargement: " + resultat/fois);
+								
+							}else{
+								Console.WriteLine("parametre -time : entier attendu");
+							}
+					}
+					else{
+	      				
+						if(result){
+							
+							for(int i=0;i<fois;i++){
+								
+								long milliseconds1 = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+								
+								try{
+									prog.getContentByUrl(args[2]);
+								}
+								catch(Exception ex){
+									Console.WriteLine(ex);
+								}
+								
+								long milliseconds2 = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+								long resultat = milliseconds2 - milliseconds1;
+								
+								Console.WriteLine("temps test numero" + i + " : " + resultat);
+							}
+							
+						}else{
+							Console.WriteLine("parametre -time : entier attendu");
+						}
+					}	
+				}
+			}
+		}
+	}
+}
---
+++ etape2/Students/Pouillat-Gaëtan__Rawinderjeet-Singh/nget-v2/nget-v2_librairie/Test.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Griev_000
+ * Date: 15/05/2015
+ * Time: 18:04
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NUnit.Framework;
+using nget_v2;
+
+namespace nget_v2_librairie
+{
+	[TestFixture]
+	public class Test
+	{
+		[Test]
+		public void Should_show_the_content_of_a_page()
+		{
+			Program prog = new Program();
+			string content = prog.getContentByUrl("http://url-bidon.fr");
+		    Assert.That(content, Is.EqualTo("<h1>hello</h1>"));
+		}
+		
+		[Test]
+		public void should_return_content_page()
+		{
+			Program prog = new Program();
+			string content = "";
+			content = prog.getContentByUrl("http://www.google.fr");
+			
+			Assert.IsNotEmpty(content);
+		}
+		
+		[Test]
+		public void should_write_file()
+		{
+			
+		}
+	}
+}
