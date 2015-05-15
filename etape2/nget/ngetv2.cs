81032414d99c82d7e9e5bbc842e5c3688adeadec
ChoucheneH <hdili.chouchene@gmail.com>
2015-05-15 18:50:06 +0200

    no message

--- etape2/nget/ngetv2.cs
+++ etape2/nget/ngetv2.cs
@@ -12,6 +12,9 @@ using System.Net;
 using System.Diagnostics;
 namespace nget
 {
+	public class  AppendAllText {
+		
+	}
 	/// <summary>
 	/// Description of ngetv2.
 	/// </summary>
@@ -28,72 +31,88 @@ namespace nget
 				 * 
 				 */
 				if (args[0].Equals("get")){
-					/*
-					 tester Si la commande get a de paramétres
-					 */
-					if(string.IsNullOrEmpty(args[1])|string.IsNullOrEmpty(args[2])){
-						Console.WriteLine("Les paramétres de commande Get Invalide");
-						/*
-					 tester Si la commande get a de 2 paramétres
-					 exepmle : get -url "aa"
-						 */
-					}else{
-						if(args.Length==3 &&args[1].Equals("-url")) {
-							string sURL=args[2];
-							WebClient client=new WebClient();
-							string value =client.DownloadString(sURL);
-							Console.WriteLine(value);
-							
-							Console.ReadLine();
-						}else if(args.Length==3) {
-							Console.WriteLine("les parmamétres de get Erronées");
-							
-						}else{
-							if(string.IsNullOrEmpty(args[3])|string.IsNullOrEmpty(args[4])){
-								Console.WriteLine("Les paramétre de commande Get save Invalide");
-							}else{
-								string path=args[4];
-								string sURL=args[2];
-								WebClient client=new WebClient();
-								string value =client.DownloadString(sURL);
-								
-								if(!File.Exists(path)){
-									File.AppendAllText(args[4], value);
-									
-								}
-								Console.WriteLine("creation de fichier valide");
-							}
-						}
-					}
+					ArgIsGet(args);
 					/*
 					 Tester Si la commande est Test
 					 */
 				}else if(args[0].Equals("test")){
-					Console.WriteLine("Test");
-					if(string.IsNullOrEmpty(args[1])|string.IsNullOrEmpty(args[2])|string.IsNullOrEmpty(args[3])|string.IsNullOrEmpty(args[4])){
-						Console.WriteLine("Les paramétre de commande Test Invalide");
-					}else if(args.Length==5 & args[1].Equals("-url")&args[3].Equals("-times")){
-						int numEssai=int.Parse(args[4]);
-						int i=0;
-						string sURL=args[2];
-						if(int.Parse(args[4])>0){
-							while(i<numEssai){
-								Stopwatch stopwatch = Stopwatch.StartNew();
-								WebClient client=new WebClient();
-								string value =client.DownloadString(sURL);
-								stopwatch.Stop();
-								i++;
-								Console.WriteLine("le chargement N° :{0}:{1} ms",i,stopwatch.Elapsed.TotalMilliseconds);
-							}
-						}else{
-							Console.WriteLine("Le nombre doit être positive");
-						}
-					}
 					
+					ArgIsTest(args);
 				}else{
 					Console.WriteLine("Commande Invalide");
 				}
 			}
 		}
+		
+		string ArgIsGet(string[] args){
+			string resultat= string.Empty;
+			/*
+					 tester Si la commande get a de paramétres
+			 */
+			if(string.IsNullOrEmpty(args[1])|string.IsNullOrEmpty(args[2])){
+				resultat="paramétres de commande Get Invalide";
+				/*
+					 tester Si la commande get a de 2 paramétres
+					 exepmle : get -url "aa"
+				 */
+			}else{
+				if(args.Length==3 &&args[1].Equals("-url")) {
+					string sURL=args[2];
+					WebClient client=new WebClient();
+					string value =client.DownloadString(sURL);
+					resultat=value;
+					
+					Console.ReadLine();
+				}else if(args.Length==3) {
+					resultat="les parmamétres de get Erronées";
+					
+				}else{
+					if(string.IsNullOrEmpty(args[3])|string.IsNullOrEmpty(args[4])){
+						resultat="Les paramétre de commande Get save Invalide";
+					}else{
+						string path=args[4];
+						string sURL=args[2];
+						WebClient client=new WebClient();
+						string value =client.DownloadString(sURL);
+						
+						if(!File.Exists(path)){
+							File.AppendAllText(args[4], value);
+							
+						}
+						resultat="creation de fichier valide";
+					}
+				}
+			}
+			return resultat;
+		}
+		
+		
+		
+		string ArgIsTest(string[] args){
+			string resultat=string.Empty;
+			
+			resultat="Test";
+			if(string.IsNullOrEmpty(args[1])|string.IsNullOrEmpty(args[2])|string.IsNullOrEmpty(args[3])|string.IsNullOrEmpty(args[4])){
+				resultat="Les paramétre de commande Test Invalide";
+			}else if(args.Length==5 & args[1].Equals("-url")&args[3].Equals("-times")){
+				int numEssai=int.Parse(args[4]);
+				int i=0;
+				string sURL=args[2];
+				if(int.Parse(args[4])>0){
+					while(i<numEssai){
+						Stopwatch stopwatch = Stopwatch.StartNew();
+						WebClient client=new WebClient();
+						string value =client.DownloadString(sURL);
+						stopwatch.Stop();
+						i++;
+						resultat="le chargement N° :"+i+":"+stopwatch.Elapsed.TotalMilliseconds +" ms" ;
+					}
+				}else{
+					resultat="Le nombre doit être positive";
+				}
+			}
+			return resultat;
+		}
 	}
+	
 }
--- etape2/nget_test/MyClass.cs
+++ etape2/nget_test/MyClass.cs
@@ -17,9 +17,9 @@ namespace nget_test
 	
 	public class MyClass
 	{
-	
+		[Test]
 		public void should_print_help_if_no_arguments(){
-			var args = new []{"Test"};
+			var args = new []{"test","-url","http://api.openweathermap.org/data/2.5/weather?q=paris&units=metric","-times" ,"1"};
 			var expected ="requete vide";
 			var sb = new StringBuilder();
 			var writer = new StringWriter();
