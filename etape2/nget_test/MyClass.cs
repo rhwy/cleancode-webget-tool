1ede56d37d3ac48b6e77b6315e6d81ee3981e468
ChoucheneH <hdili.chouchene@gmail.com>
2015-05-15 18:28:00 +0200

    Test args

---
+++ etape2/nget_test/MyClass.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Crée par SharpDevelop.
+ * Utilisateur: hedhili
+ * Date: 15/05/2015
+ * Heure: 18:08
+ * 
+ * Pour changer ce modèle utiliser Outils | Options | Codage | Editer les en-têtes standards.
+ */
+using System;
+using NUnit.Framework;
+using System.Text;
+using System.IO;
+using nget;
+namespace nget_test
+{
+	[TestFixture]
+	
+	public class MyClass
+	{
+	
+		public void should_print_help_if_no_arguments(){
+			var args = new []{"Test"};
+			var expected ="requete vide";
+			var sb = new StringBuilder();
+			var writer = new StringWriter();
+			Console.SetOut(writer);
+		
+			Program.Main(args);
+			var resultat = sb.ToString();
+			Assert.AreEqual(expected,resultat);
+			
+			
+		}
+	}
+}
--- etape2/nget_test/Properties/AssemblyInfo.cs
+++ etape2/nget_test/Properties/AssemblyInfo.cs
@@ -9,11 +9,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("nget")]
+[assembly: AssemblyTitle("nget_test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("nget")]
+[assembly: AssemblyProduct("nget_test")]
 [assembly: AssemblyCopyright("Copyright 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
