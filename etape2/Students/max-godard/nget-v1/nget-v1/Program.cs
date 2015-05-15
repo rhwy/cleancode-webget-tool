4a1c26234dfb4e86ce8322f3db4a8ae99cd22cb3
crazymx <mx.godard@gmail.com>
2015-05-15 18:48:05 +0200

    Refactoring

--- etape2/Students/max-godard/nget-v1/nget-v1/Program.cs
+++ etape2/Students/max-godard/nget-v1/nget-v1/Program.cs
@@ -12,6 +12,7 @@ using System.Net;
 using System.Diagnostics;
 
 
+
 namespace nget_v1
 {
 	class Program
@@ -23,89 +24,37 @@ namespace nget_v1
 		public const int ARGS_CMD_SAVE = 3;
 		public const int ARGS_PATH = 4;
 		public const int ARGS_CMD_AVG = 5;
-		
-		public static String getUrlContent(String url){
-			try{
-				return (new WebClient().DownloadString(url));
-			}catch(Exception e){
-				return ("Erreur lecture url: " + e.Message);
-			}
-		}
-		
-		public static void saveUrl(String url, String path){
-			try{
-				File.WriteAllText(path, getUrlContent(url));
-			}catch(Exception e){
-				Console.WriteLine("Erreur écriture fichier: " + e.Message);
-			}
-		}
-		
-		public static void testUrlLoadingTime(String url, int nbTime){			
-			var sw = new Stopwatch();
-			
-			for(int cpt=1; cpt<nbTime+1; cpt++){
-				sw.Start();
-				try{
-					String client = new WebClient().DownloadString(url);
-				}catch(Exception e){
-					Console.WriteLine(e.Message);
-				}
-				sw.Stop();
-				
-				Console.WriteLine("Temps" + (cpt) + ":" + sw.ElapsedMilliseconds + "ms");
-				sw.Restart();
-			}
-		}
-		
-		public static void testAvgTime(String url, int nbTime){
-			var sw = new Stopwatch();
-			int avg=0;
-			Console.WriteLine("Calcul de la moyenne en cours . . .");
-			for(int cpt=1; cpt<nbTime+1; cpt++){
-				sw.Start();
-				String client = new WebClient().DownloadString(url);
-				sw.Stop();
-				avg += Convert.ToInt32(sw.ElapsedMilliseconds);
-				sw.Restart();
-			}
-			avg /= nbTime;
-			Console.WriteLine("Moyenne de temps de chargement: " + avg);
-		}
-		
+
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("nget.exe v1.0");
-			
+			Console.WriteLine("nget.exe v2.0");			
 			if(args.Length < 3 || args.Length > 6){
 				Console.Write("Nombre d'arguments incorrect . . . ");
 				Console.ReadKey(true);
 				return;
-			}
-			
-			String url = args[ARGS_URL];
-			
+			}			
+			var url = new Url(args[ARGS_URL]);
 			switch(args[0].ToLower()){
 				case "get":
 					if(args.Length > 4 && args[ARGS_CMD_URL]=="-url" && args[ARGS_CMD_SAVE] == "-save"){
 						String path = args[ARGS_PATH];
-						saveUrl(url, path);
+						url.saveContent(path);
 					}
 					else if(args[ARGS_CMD_URL]=="-url"){
-						Console.WriteLine(getUrlContent(url));
+						Console.WriteLine(url.getContent());
 					}
 					break;
 					
 				case "test":
 					if(args.Length == 5 && args[ARGS_CMD_URL]=="-url" && args[ARGS_CMD_TIME]=="-times"){
 						int nbTime = Convert.ToInt32(args[ARGS_NBTIME]);
-						testUrlLoadingTime(url, nbTime);
+						url.testLoadingTime(nbTime);
 					}else if(args.Length == 6 && args[ARGS_CMD_URL]=="-url" && args[ARGS_CMD_TIME]=="-times" && args[ARGS_CMD_AVG]=="-avg"){
 						int nbTime = Convert.ToInt32(args[ARGS_NBTIME]);
-						testAvgTime(url, nbTime);
+						url.testAvgTime(nbTime);
 					}else{
 						Console.WriteLine("Arguments incorrects");
 					}
-					
 					break;
 				default:
 					Console.WriteLine("Commande inconnue");
---
+++ etape2/Students/max-godard/nget-v1/nget-v1/Url.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Max
+ * Date: 15/05/2015
+ * Time: 17:55
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Net;
+using System.Diagnostics;
+
+namespace nget_v1
+{
+	/// <summary>
+	/// Description of Url.
+	/// </summary>
+	public class Url
+	{
+		string address;
+		string content;
+		
+		public string Address{
+			get{
+				return address;
+			}
+		}
+		
+		public Url(string ad)
+		{
+			address = ad;
+			content = getContent();
+		}
+		
+		public string getContent(){
+			try{
+				return (new WebClient().DownloadString(address));
+			}catch(Exception e){
+				return ("Erreur lecture url: " + e.Message);
+			}
+		}
+		
+		public string saveContent(string path){
+			string result="";
+			try{
+				result=getContent();
+				File.WriteAllText(path, getContent());
+			}catch(Exception e){
+				result="Erreur écriture fichier: " + e.Message;
+			}
+			return result;
+		}
+		
+			
+		public string testLoadingTime(int nbTime){			
+			string result="";
+			var sw = new Stopwatch();
+			for(int cpt=1; cpt<nbTime+1; cpt++){
+				swManager(sw);
+				result+="Temps" + (cpt) + ":" + sw.ElapsedMilliseconds + "ms";
+				Console.WriteLine(result);
+				sw.Restart();
+			}
+			 return result;
+		}
+		
+		public string testAvgTime(int nbTime){
+			string result = "";
+			var sw = new Stopwatch();
+			int avg=0;
+			Console.WriteLine("Calcul de la moyenne en cours . . .");
+			for(int cpt=1; cpt<nbTime+1; cpt++){
+				swManager(sw);
+				avg += Convert.ToInt32(sw.ElapsedMilliseconds);
+				sw.Restart();
+			}
+			avg /= nbTime;
+			result = "Moyenne de temps de chargement: " + avg;
+			Console.WriteLine(result);
+			return result;
+		}
+		
+		public string swManager(Stopwatch sw)
+		{
+			string res="";
+			sw.Start();
+			res=getContent();
+			sw.Stop();
+			return res;
+		}
+		
+	}
+}
