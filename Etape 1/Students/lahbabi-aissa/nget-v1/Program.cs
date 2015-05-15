caad749dd875f89287476183e1b15d8d0370a197
Aissa <aissa_git@installtech.fr>
2015-05-15 17:41:35 +0200

    ReadMe + Copie du projet Etape 1 => Etape 2

---
+++ Etape 1/Students/lahbabi-aissa/nget-v1/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Crée par SharpDevelop.
+ * Utilisateur: Aissa
+ * Date: 07/04/2015
+ * Heure: 11:42
+ * 
+ * Pour changer ce modèle utiliser Outils | Options | Codage | Editer les en-têtes standards.
+ */
+using System;
+using System.Net;
+
+namespace nget_v1
+{
+	class Program
+	{
+		public static void printHelp() {
+			Console.Write("Utilisataion : <get|test> -url <url> <-times|-save> <nb_of_loads> -avg");
+			Console.ReadKey(true);
+		}
+		
+		public static void Main(string[] args)
+		{
+			// Incorrect use
+			if (args.Length < 2 || (args[0] != "get" && args[0] != "test")) {
+				printHelp();
+				return;
+			}
+			
+			
+			if (args[0] == "get") {
+				// Get content of URL
+				WebFile web = new WebFile(args[2]);
+				
+				if (args.Length > 4 && args[3] == "-save" ) {
+					// Download it into specified filename
+					web.download(args[4]);
+				} else {
+					// Just print it
+					web.print();
+				}
+				
+			} else if (args[0] == "test") {
+				// Test and print loadtime
+				
+				Boolean printAvg = (args.Length > 5 && args[5] == "-avg");
+				
+				WebFile web = new WebFile(args[2]);
+				web.times(Convert.ToInt32(args[4]), printAvg);
+				
+				
+			} else {
+				printHelp();
+				return;
+			}
+
+			
+			Console.ReadKey(true);
+		}
+		
+		
+	}
+}
---
+++ Etape 1/Students/lahbabi-aissa/nget-v1/WebFile.cs
@@ -0,0 +1,95 @@
+﻿/*
+ * Crée par SharpDevelop.
+ * Utilisateur: Aissa
+ * Date: 07/04/2015
+ * Heure: 12:15
+ * 
+ * Pour changer ce modèle utiliser Outils | Options | Codage | Editer les en-têtes standards.
+ */
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+
+namespace nget_v1
+{
+	/// <summary>
+	/// Description of WebFile.
+	/// </summary>
+	public class WebFile
+	{
+		string _url;
+		WebClient _client;
+		
+		public WebFile(string url)
+		{
+			_url 	= url;
+			_client = new WebClient();
+		}
+		
+		
+		public void print() {
+			try {
+			   string content = _client.DownloadString(_url);
+			   Console.Write(content);
+			} catch (WebException){
+				Console.WriteLine("URL inaccessible");
+			} catch (Exception e){
+				Console.WriteLine(e);
+			}
+		}
+		
+		public void download(string path) {
+			
+			try {
+			   string content = _client.DownloadString(_url);
+			   File.WriteAllText(path, content);
+			   Console.WriteLine("Données sauvegardées");
+			} catch (WebException){
+				Console.WriteLine("URL inaccessible");
+			} catch (UnauthorizedAccessException){
+				Console.WriteLine("L'emplacement indiqué est inaccessible. Merci de vérifier les droits d'accès");
+			} catch (Exception e){
+				Console.WriteLine(e);
+			}
+		}
+		
+		public void times(int nb_loads, Boolean print_avg) {
+			long[] duration = new long[nb_loads];
+			Stopwatch stopwatch;
+			
+			// 1) On charge l'URL x fois 
+			for (int i = 0; i < nb_loads; i++) {
+				stopwatch = Stopwatch.StartNew();
+				
+				try {
+					_client.DownloadString(_url);
+				} catch (WebException){
+					Console.WriteLine("URL inaccessible");
+				} catch (Exception e){
+					Console.WriteLine(e);
+				}
+				
+				stopwatch.Stop();
+				duration[i] = stopwatch.ElapsedMilliseconds;
+			}
+			
+			
+			// 2) Affichage du resultat
+			if (print_avg) {
+				long moyenne = 0;
+				for (int i = 0; i < duration.Length; i++) {
+					moyenne += duration[i];
+				}
+				moyenne = moyenne / nb_loads;
+				
+				Console.WriteLine("Temps de chargement moyen : " + moyenne + " ms");
+			} else {
+				for (int i = 0; i < duration.Length; i++) {
+					Console.WriteLine(i + " => " + duration[i] + " ms");
+				}
+			}
+			
+		}
+	}
+}
