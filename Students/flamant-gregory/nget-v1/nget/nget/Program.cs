35f9fdf43a600bea24ad9f3ec1be205a9982a5a8
gregoryflamant <gw.flamant@gmail.com>
2015-06-01 20:01:06 +0200

    Ajout des fonctionnalités

---
+++ Students/flamant-gregory/nget-v1/nget/nget/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nget
+{
+    //Point d'entrée du programme
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new MenuTraitement(args);
+            Console.Read();
+        }
+    }
+}
