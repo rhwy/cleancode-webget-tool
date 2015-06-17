45bff5040c9af9596ad37d2fd205cb7cbb5bfd56
Yo911 <yehouda_arnauve@hotmail.com>
2015-06-17 16:41:25 +0200

    interfaces pour command

---
+++ Students/Arnauve_et_Iisiramen/nget/nget/IParseCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nget
+{
+    interface IParseCommand
+    {
+        void execute(string[] command);
+        void parse(string[] command);
+    }
+}
---
+++ Students/Arnauve_et_Iisiramen/nget/nget/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IExecuteCommand ec = new IExecuteCommand();
+            IParseCommand command;
+            Console.WriteLine(args[0]);
+            switch (args[0])
+            {
+                case "get":
+                    command = new GetCommand(ec);
+                    command.execute(args);
+                    break;
+                case "test":
+                    command = new GetCommand(ec);
+                    command.execute(args);
+                    break;
+            }
+        }
+    }
+}
