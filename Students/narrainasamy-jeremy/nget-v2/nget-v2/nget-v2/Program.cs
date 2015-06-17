e892d8208691b7db046a2f93341621473e46c9ce
jkn1092 <jkn1092@gmail.com>
2015-06-17 16:43:11 +0200

    Code à cleaner

--- Students/narrainasamy-jeremy/nget-v2/nget-v2/nget-v2/Program.cs
+++ Students/narrainasamy-jeremy/nget-v2/nget-v2/nget-v2/Program.cs
@@ -7,6 +7,11 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
 
 namespace nget_v2
 {
@@ -14,12 +19,76 @@ namespace nget_v2
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
 			
-			// TODO: Implement Functionality Here
-			
-			Console.Write("Press any key to continue . . . ");
+			switch(args.Length){
+				case 3:
+					if(args[0] == "get" && args[1] == "-url"){
+						Console.WriteLine(getContenuUrl(args[2]));
+					}else{
+						Console.WriteLine("Une erreur est survenue.");
+					}
+					break;
+					
+				case 5:
+					if(args[0] == "get" && args[1] == "-url" && args[3] == "-save"){
+						System.IO.File.WriteAllText(args[4],getContenuUrl(args[2]));
+					}
+					else if(args[0] == "test" && args[1] == "-url" && args[3] == "-times"){
+						var nbTest = Convert.ToInt32(args[4]);
+						
+						for(var i = 0; i < nbTest; i++){
+							Console.WriteLine(getChargementUrl(args[2]).TotalSeconds);
+						}
+						
+					}else{
+						Console.WriteLine("Une erreur est survenue.");
+					}
+					break;
+					
+				case 6:
+					if(args[0] == "test" && args[1] == "-url" && args[3] == "-times" && args[5] == "-avg"){
+						
+						var nbTest = Convert.ToInt32(args[4]);
+						var listeTemps = new List<TimeSpan>();
+						
+						for(var i = 0; i < nbTest; i++){
+							listeTemps.Add(getChargementUrl(args[2]));
+						}
+						
+						var moyenne = TimeSpan.FromSeconds(listeTemps.Average(time=>time.TotalSeconds));
+						Console.WriteLine(moyenne.TotalSeconds);
+						
+					}else{
+						Console.WriteLine("Une erreur est survenue.");
+					}
+					break;
+					
+				default:
+					Console.WriteLine("Une erreur est survenue.");
+					break;
+			}
+						
+			Console.Write("Press any key to exit . . . ");
 			Console.ReadKey(true);
+			
 		}
+		
+		public static String getContenuUrl(String url){
+			var client = new WebClient();
+	        var s = client.DownloadString(url);
+	        return s;
+		}
+		
+		public static TimeSpan getChargementUrl(String url){
+			var request = (HttpWebRequest)WebRequest.Create(url);
+			System.Diagnostics.Stopwatch timer = new Stopwatch();
+			
+			timer.Start();
+			var response = (HttpWebResponse)request.GetResponse();
+			timer.Stop();
+			
+			return timer.Elapsed;
+		}
+		
 	}
 }
