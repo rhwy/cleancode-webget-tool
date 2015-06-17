e132b9d7a7be86f16a0d4c741156f96eec8aaaef
Yo911 <yehouda_arnauve@hotmail.com>
2015-06-17 16:41:50 +0200

    impementation de l'interface commande

---
+++ Students/Arnauve_et_Iisiramen/nget/nget/GetCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nget
+{
+    class GetCommand : IParseCommand
+    {
+        private IExecuteCommand exec;
+        Dictionary<string, string> dico = new Dictionary<string, string>();
+        public GetCommand(IExecuteCommand e)
+        {
+            exec = e;
+        }
+
+        private void parse(string[] command)
+        {
+            if (command.Length != 5 || command.Length != 3) return ;
+            for(int i = 1; i < command.Length; i += 2) {
+                dico.Add(command[i], command[i + 1]);
+            }
+        }
+
+        public void execute(string[] command) {
+            parse(command);
+            
+        }
+    }
+}
---
+++ Students/Arnauve_et_Iisiramen/nget/nget/TestCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nget
+{
+    class TestCommand
+    {
+        private IExecuteCommand exec;
+        Dictionary<string, string> dico = new Dictionary<string, string>();
+
+        public TestCommand(IExecuteCommand e)
+        {
+            exec = e;
+        }
+
+        private void parse(string[] command)
+        {
+            if (command.Length != 5 || command.Length != 6) return;
+            
+            for (int i = 1; i < command.Length; i += 2)
+            {
+                if (i == 5)
+                    dico.Add(command[i], "true");
+                else
+                    dico.Add(command[i], command[i + 1]);
+            }
+        }
+
+        public void execute(string[] command)
+        {
+            parse(command);
+            exec.execute("test", dico);
+        }
+    }
+}
